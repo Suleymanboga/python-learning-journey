ef7f9d6c8de5ff244d9b9f168a003d5ed6ada9b3
Suleymanboga <suloboha3@gmail.com>
2025-12-15 19:22:56 +0300

    Day 3: Added random password generator and log analyzer tools

--- password_generator.py
+++ password_generator.py
@@ -2,10 +2,12 @@
 Generate random passwords of the desired length"""
 import random
 import string
-
-password_length = int(input("Please enter the desired password length: "))
-password= ""
-all_chars = string.ascii_letters + string.digits + string.punctuation
-for i in range(password_length):
-    password += random.choice(all_chars)
-print("Password:", password)
+try:
+    password_length = int(input("Please enter the desired password length: "))
+    password= ""
+    all_chars = string.ascii_letters + string.digits + string.punctuation
+    for i in range(password_length):
+        password += random.choice(all_chars)
+    print("Password:", password)
+except ValueError:
+    print("Please enter a number")
